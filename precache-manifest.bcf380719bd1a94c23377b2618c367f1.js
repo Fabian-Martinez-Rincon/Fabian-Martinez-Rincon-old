63611018d22438c5a3f34c960e2617722bf054ca
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-11-29 00:00:12 -0300

    Updates

---
+++ precache-manifest.bcf380719bd1a94c23377b2618c367f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee36154bdc94740d44d860e22630a4c4",
+    "url": "/PortfolioReact/index.html"
+  },
+  {
+    "revision": "6d4d107ecc2a25e29dd6",
+    "url": "/PortfolioReact/static/js/2.e855473f.chunk.js"
+  },
+  {
+    "revision": "683977c346077458667292274cdb7595",
+    "url": "/PortfolioReact/static/js/2.e855473f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6e76ca5dcdf40858e9",
+    "url": "/PortfolioReact/static/js/main.4042dbb5.chunk.js"
+  },
+  {
+    "revision": "2d70b3898089e090d095",
+    "url": "/PortfolioReact/static/js/runtime-main.0f0a46b8.js"
+  },
+  {
+    "revision": "60613b71a10bfeb56a6866b2add35f41",
+    "url": "/PortfolioReact/static/media/img.60613b71.png"
+  }
+]);
