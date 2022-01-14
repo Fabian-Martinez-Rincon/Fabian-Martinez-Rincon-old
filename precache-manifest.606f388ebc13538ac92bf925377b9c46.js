a8f094af1389747e14f9b33c2259eba5aa7765aa
Fabo xw <fabianmartinezrincon.123@gmail.com>
2022-01-14 18:40:32 -0300

    Updates

---
+++ precache-manifest.606f388ebc13538ac92bf925377b9c46.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46319f8a08e47f5f18d76ea7084fb112",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28d006659ed5faee0212",
+    "url": "/static/js/2.11c6b0d4.chunk.js"
+  },
+  {
+    "revision": "683977c346077458667292274cdb7595",
+    "url": "/static/js/2.11c6b0d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23853e0943af7a6bd483",
+    "url": "/static/js/main.d6f23c19.chunk.js"
+  },
+  {
+    "revision": "16eed37f38735969b0de",
+    "url": "/static/js/runtime-main.5f1a8f4d.js"
+  },
+  {
+    "revision": "34ea2f3b1927b856a129a3c533809f2f",
+    "url": "/static/media/1.34ea2f3b.png"
+  },
+  {
+    "revision": "d4e53d24bdccc43bbe74f4effc6e471e",
+    "url": "/static/media/2.d4e53d24.png"
+  },
+  {
+    "revision": "594a1cd65f230719d2b498dbe863e89e",
+    "url": "/static/media/3.594a1cd6.png"
+  },
+  {
+    "revision": "0246c446d253cea0f06c028adf4cc402",
+    "url": "/static/media/4.0246c446.png"
+  },
+  {
+    "revision": "3d1393b07cf50d29ae2e34fd62cc1350",
+    "url": "/static/media/5.3d1393b0.png"
+  },
+  {
+    "revision": "25188ca7301c627c24370df844c27521",
+    "url": "/static/media/6.25188ca7.png"
+  },
+  {
+    "revision": "84c990a41916983cc61942e6c831d2da",
+    "url": "/static/media/7.84c990a4.png"
+  },
+  {
+    "revision": "0d83b3e71b7f918f5d661e33f2adcbdb",
+    "url": "/static/media/8.0d83b3e7.png"
+  },
+  {
+    "revision": "0172e65c85b51b0982afca88211866d1",
+    "url": "/static/media/9.0172e65c.PNG"
+  },
+  {
+    "revision": "919f4f7355b79eaa9ba71b187ca47c1d",
+    "url": "/static/media/foto2.919f4f73.jpg"
+  },
+  {
+    "revision": "4ab297b596df06689da857c10b2f6b11",
+    "url": "/static/media/img2.4ab297b5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PortfolioReact/precache-manifest.9856c2f375f0f07ebc798193e3e2f2d0.js"
+  "/precache-manifest.606f388ebc13538ac92bf925377b9c46.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PortfolioReact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
