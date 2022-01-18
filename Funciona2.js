683c43f19663d89fad0fbd0ce1835a2a2d5a3f7b
Fabo xw <fabianmartinezrincon.123@gmail.com>
2022-01-18 13:36:42 -0300

    Funciona2

---
+++ Funciona2.js
@@ -0,0 +1,49 @@
+// app.js
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to React Router!</h1>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+export default App;
